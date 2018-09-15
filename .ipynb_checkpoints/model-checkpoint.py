543518ab5a569d65004f441346f6b1b9cf013d7e
Peter C <peter@labone.tech>
2018-09-15 14:59:32 -0600

    tested model against many dataset sizes

---
+++ .ipynb_checkpoints/model-checkpoint.py
@@ -0,0 +1,193 @@
+# Model Chaaracter Level Script
+
+import csv
+from datetime import datetime
+from IPython.display import SVG
+from keras import backend as K
+from keras.callbacks import EarlyStopping, TensorBoard
+from keras.layers import Dense, Input, LSTM
+from keras.models import Model
+from keras.optimizers import RMSprop
+from keras.preprocessing.text import text_to_word_sequence
+from keras.utils import plot_model
+from keras.utils.vis_utils import model_to_dot
+import numpy as np
+import os
+import sys
+
+# hyper parameters
+activations = 128
+batch_size = 50
+epochs = 10
+learning_rate = 0.01
+training_ratio = 0.3
+
+# output
+output_dir = 'data/'
+charset_file = '{}charset.csv'.format(output_dir)
+dataset_file = '{}dataset.csv'.format(output_dir)
+
+# to use GPU
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
+
+# verify that a gpu is listed
+K.tensorflow_backend._get_available_gpus()
+
+def deprocessPrediction(ix_to_char, prediction):
+    index = np.argmax(prediction)
+    char = ix_to_char[index]
+    
+    return char
+    
+def generateCharacterConverters(chars):
+    char_to_ix = { ch:i for i,ch in enumerate(sorted(chars)) }
+    ix_to_char = { i:ch for i,ch in enumerate(sorted(chars)) }
+    
+    return char_to_ix, ix_to_char
+    
+def generateXYDatasets(char_to_ix, dataset, n_examples, n_charset, max_char_n):
+    # create training input
+    x_dataset = np.zeros((n_examples, max_char_n-1, n_charset), dtype='float32')
+    
+    # create training input
+    y_dataset = np.zeros((n_examples, n_charset), dtype='float32')
+    
+    # fill input training set with word sequences, where words are one-hot encoded
+    for li, line in enumerate(dataset):
+        for ci, char in enumerate(line[:-1]):
+            index = char_to_ix[char]
+            x_dataset[li][ci][index] = 1
+            
+    # create training output
+    for li, line in enumerate(dataset):
+        char = line[-1]
+        index = char_to_ix[char]
+        y_dataset[li][index] = 1
+        
+    return x_dataset, y_dataset
+    
+def preprocessExample(char_to_ix, example, n_chars_set):
+    chars = list(example)
+    n_sample_chars = len(chars)
+
+    preprocessed_example = np.zeros((1, n_sample_chars, n_chars_set), dtype='float32')
+
+    for ci, char in enumerate(chars):
+        index = char_to_ix[char]
+        preprocessed_example[0][ci][index] = 1
+
+    return preprocessed_example
+    
+def sample_predictions(preds, temperature=0.5):
+    # helper function to sample an index from a probability array
+    preds = np.asarray(preds).astype('float64')
+    preds = np.log(preds) / temperature
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    probas = np.random.multinomial(1, preds, 1)
+    return probas
+    
+with open(charset_file, 'r') as csv_file:
+    reader = csv.reader(csv_file, delimiter=",")
+    charset = []
+    for row in reader:
+        charset.append(row[0])
+
+with open(dataset_file, 'r') as csv_file:
+    reader = csv.reader(csv_file, delimiter=",")
+    dataset = []
+    for row in reader:
+        dataset.append(row)
+        
+# create dictionarys
+char_to_ix, ix_to_char = generateCharacterConverters(charset)
+n_charset = len(charset)
+
+print("Number of characters: {}".format(n_charset))
+print("ix_to_char: {}".format(ix_to_char))
+print("char_to_ix: {}".format(char_to_ix))
+
+# create training input and output
+n_examples = len(dataset)
+max_char_n = len(dataset[0])
+x_dataset, y_dataset = generateXYDatasets(char_to_ix, dataset, n_examples, n_charset, max_char_n)
+print("Number of examples: {}".format(n_examples))
+print("max characters: {}".format(max_char_n))
+
+x_example = x_dataset[2] 
+
+x_example_string = []
+for woh in x_example:
+    char = deprocessPrediction(ix_to_char, woh)
+    x_example_string.append(char)
+x_example_string_formatted = "".join(x_example_string)
+
+print("x_example shape: {}".format(x_example.shape))
+print("x_example one-hot: {}".format(x_example))
+print("x_example: {}".format(x_example_string_formatted))
+
+y_example = y_dataset[2]
+
+char = deprocessPrediction(ix_to_char, y_example)
+
+print("y_example shape: {}".format(y_example.shape))
+print("y_example one-hot: {}".format(y_example))
+print("y_example: {}".format(char))
+
+model_input = Input(shape=(None, n_charset))
+x = LSTM(activations)(model_input)
+x = Dense(n_charset, activation='softmax')(x)
+
+model = Model(inputs=model_input, outputs=x)
+
+optimizer = RMSprop(lr=learning_rate)
+model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+
+# generate logging variables
+variant = '{}ex-{}a-{}b-{}c'.format(n_examples, activations, batch_size, max_char_n)
+timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+log_dir = 'logs/{}-{}'.format(variant, timestamp)
+diagram_dir = 'diagram/{}-{}'.format(variant, timestamp)
+
+# draw a model diagram and save it to disk
+plot_model(model, to_file=diagram_dir)
+SVG(model_to_dot(model).create(prog='dot', format='svg'))
+
+model.summary()
+
+# set up callbacks
+early = EarlyStopping(monitor='val_acc',
+                      min_delta=0,
+                      patience=10,
+                      verbose=1,
+                      mode='auto')
+                      
+model.fit(x_dataset, 
+          y_dataset, 
+          batch_size=batch_size, 
+          epochs=epochs, 
+          shuffle=True,
+          validation_split=training_ratio,
+          callbacks=[early, TensorBoard(log_dir=log_dir)])                      
+          
+example = 'sweet dreams are made of these'          
+
+# convert example to a sequence of one-hot encoded chars
+preprocessed_example = preprocessExample(char_to_ix, example, n_charset)
+
+prediction = model.predict(preprocessed_example)
+
+char = deprocessPrediction(ix_to_char, prediction[0])
+
+print("Prediction: {}".format(char))
+
+sys.stdout.write(example)
+
+for i in range(500):
+    prediction = model.predict(preprocessed_example, verbose=0)[0]
+    sampled_prediction = sample_predictions(prediction)
+    next_char = deprocessPrediction(ix_to_char, sampled_prediction[0])
+    preprocessed_example[0][:-1] = preprocessed_example[0][1:]
+    preprocessed_example[0][-1] = sampled_prediction
+    sys.stdout.write(next_char)
+    sys.stdout.flush()
---
+++ results.md
@@ -0,0 +1,61 @@
+# Results
+
+## 100000ex-128a-50b-20c
+sweet dreams are made of these
+i wanna i can't be free)
+and i can't
+i am
+she's gonna feel the time
+when i am
+the way the sigh that the time and from anywording of my all i was you see your heart slawing
+it was me christ
+all on the sound
+and i love you thing that and the someone
+i feel down the sunsed and hemplass
+and don't have and when you was to the smilestay we try thing you didn't the someone you would be fre
+e)
+something it is that was to sleep
+it was the way that my smile and for you but there's some out then a feeling
+
+## 300000ex-128a-50b-20c
+sweet dreams are made of these you saw you say
+and i never knew, you like you to have you to heare
+the knew the love can you say,
+seems away that don't let your love to see the love that we find to me
+to have you strengy with you to me
+the trients of the stars with your love didn't let your love to feel the light the knew with you
+we'll hold in the stranged
+everything that you got to be your love in the stork her strippen baby
+forget your fafe the way they wanng
+
+## 400000ex-128a-50b-20c
+sweet dreams are made of these we didn't see you one sight
+stay, the thing i can should see me
+this is it with me
+i love you
+when you can see believering that on the sun better let me see out on the love
+it's the more i think i stay could care it's me to last to you
+i really you stay
+we things you stay away
+i'm not the sun but i got to  the baby
+but i know the feeling her man
+i can love you it story to cry                    
+i want to the love is we can i'm been to the love of a loving and something will have to love you
+
+## 500000ex-128a-50b-20c
+sweet dreams are made of these but is lost        
+to thing the bughing to me                        
+one all the far rain me  
+everything it around for me it that you gonna the last the end the and i want to be                  
+like the sunce is sham, the ladine the heart the chance the thing the world and the sun world        
+you come to her the way  
+i see a lost             
+when the love it the sund leave                   
+this in the wing the heart the baby        
+
+## 1000000ex-128a-50b-20c
+sweet dreams are made of thesesw  d tht t we a to  sfhedt sh t bn o  ing   enofam sejt y we uf t you tathbe dsal du  two th ii  ml i i  ve  i s'rl t ti   be tasee taor c' s b te bej  yousuenronid s mvl t sr waos     meo yewsoo bqa w t h pstn t t ccyui t cnmoho yo u ont,
+m tio  ali t whap an t no po n tey al sghi          
+y idm t tu !   athen  svej g                        
+id hah hai htde me to yl ia  it  swr                
+t sgmh ato ude a aoer temes y t da o av o f a sou i iioug d otbi t ma as se' f no m s, t i t p o ola  ho yli s w  yb eraee b
