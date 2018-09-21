ef973bdd85234c8b54dad0c9e6c659e0660359fc
Peter C <peter@labone.tech>
2018-09-20 21:43:31 -0600

    made generate script more usable

--- README.md
+++ README.md
@@ -9,6 +9,9 @@ Details can be found with it's accompanying [blog post](http://labone.tech/90s-p
 2. Place it at `data/dataset.csv`
 3. Run the `create_corpus.py` to generate the corpus.
 4. Run the `model.py` to train the model.
-5. Run the `generate_lyrics.py` generate lyrics.
-
+5. Run the `generate_lyrics.py "some input" 500 'model/100000ex-128a-50b-20c-2018-09-19 19:41:58.hdf5' 'data/charset.csv'` generate lyrics.
+    - "some input" being the starting string
+    - 500 being the number of chars to generate
+    - 'model/100000ex-128a-50b-20c-2018-09-19 19:41:58.hdf5' being the model file
+    - 'data/charset.csv' being the charset array file
 peace. love. spice up your life.
--- generate_lyrics.py
+++ generate_lyrics.py
@@ -5,15 +5,11 @@ import numpy as np
 import os
 import sys
 
-# hyper parameters
-activations = 128
-example = 'sweet dreams are made of these'
-n_chars = 500
-
-# output
-output_dir = 'data/'
-charset_file = '{}charset.csv'.format(output_dir)
-model_dir = 'model/100000ex-128a-50b-20c-2018-09-19 19:41:58.hdf5'
+# inputs
+model_input = sys.argv[1]
+n_chars = int(sys.argv[2])
+model_dir = sys.argv[3]
+charset_file = sys.argv[4]
 
 # to use GPU
 os.environ["CUDA_VISIBLE_DEVICES"]="0"
@@ -34,17 +30,17 @@ def generateCharacterConverters(chars):
     
     return char_to_ix, ix_to_char
 
-def preprocessExample(char_to_ix, example, n_chars_set):
-    chars = list(example)
+def preprocessInput(char_to_ix, input, n_chars_set):
+    chars = list(input)
     n_sample_chars = len(chars)
 
-    preprocessed_example = np.zeros((1, n_sample_chars, n_chars_set), dtype='float32')
+    preprocessed_input = np.zeros((1, n_sample_chars, n_chars_set), dtype='float32')
 
     for ci, char in enumerate(chars):
         index = char_to_ix[char]
-        preprocessed_example[0][ci][index] = 1
+        preprocessed_input[0][ci][index] = 1
 
-    return preprocessed_example
+    return preprocessed_input
     
 def sample_predictions(preds, temperature=0.5):
     # helper function to sample an index from a probability array
@@ -63,29 +59,29 @@ with open(charset_file, 'r') as csv_file:
         charset.append(row[0])
         
 # Generate Charset Dictionaries
-# create dictionarys
 char_to_ix, ix_to_char = generateCharacterConverters(charset)
 n_charset = len(charset)
-
-print("Number of characters: {}".format(n_charset))
-print("ix_to_char: {}".format(ix_to_char))
-print("char_to_ix: {}".format(char_to_ix))
 
 # Load Model
 model = load_model(model_dir)
 
 # Generate a sequence from a sequence
-# convert example to a sequence of one-hot encoded chars
-preprocessed_example = preprocessExample(char_to_ix, example, n_charset)
+# convert input to a sequence of one-hot encoded chars
+preprocessed_input = preprocessInput(char_to_ix, model_input, n_charset)
 
-sys.stdout.write(example)
+result = model_input
 
 for i in range(n_chars):
-    prediction = model.predict(preprocessed_example, verbose=0)[0]
+    prediction = model.predict(preprocessed_input, verbose=0)[0]
     sampled_prediction = sample_predictions(prediction)
     next_char = deprocessPrediction(ix_to_char, sampled_prediction[0])
-    preprocessed_example[0][:-1] = preprocessed_example[0][1:]
-    preprocessed_example[0][-1] = sampled_prediction
-    sys.stdout.write(next_char)
-    sys.stdout.flush()
-print()        
+    preprocessed_input[0][:-1] = preprocessed_input[0][1:]
+    preprocessed_input[0][-1] = sampled_prediction
+    result += next_char
+
+sys.stdout.write("\nRAD NEW 90s POP LYRICS")
+sys.stdout.write("\n======================")
+sys.stdout.write("\n\n")
+sys.stdout.write(result)
+sys.stdout.write("\n")
+sys.stdout.write("\n======================\n")
